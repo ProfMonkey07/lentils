be9412565ad1de0b57f94b8feaa559a9b402242e
Sofia Adair <flowerandbelle@gmail.com>
2025-04-02 21:04:09 -0700

    I think this might work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,22 +10,43 @@
       />
     </header>
     <body>
-      <h1>Lentils To Ladles Cozy Soup Kitchen</h1>
       <div id="about">
         <section>
-          <h2>Our Story</h2>
-          <p class="text">
-            At Lentils to Ladles, we believe that good, healthy food starts with
-            simple practices and good farming.
+          <h1>Lentils To Ladles Soup Kitchen</h1> 
+          <p id="desc" class="text"> 
+            Experience our farm-to-table dining with locally sourced ingredients and a feeling of comfort in every bite.
           </p>
-          <button class="text" type="button">
-            <router-link to="/about">Learn More</router-link>
-          </button>
+          <button class="text" type="button"><router-link to="/about">Learn More</router-link></button>
         </section>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqUbmGH62AaVUIYoyhLobo1rYuDS06tCx_Nw&s"
-          alt="cozy kitchen"
-        />
+        <div class="slidecont">
+          <div class="Slide">
+            <img id="soup" src="../assets/soup.jpg" alt="bowl of soup">
+            <div id="farm" class="text">
+              <h4>Farm To Table Experience</h4>
+              <p>
+                Locally sourced ingredients picked and prepared with care.
+              </p>
+            </div>
+          </div>
+          <div class="Slide">
+            <img id="chef" src="../assets/chef.jpg" alt="Chef preparing soup">
+            <div id="hand" class="text">
+              <h4>Hand Crafted </h4>
+              <p>
+                Our chefs bring years of experience and passion, ensuring each bowl is packed with flavor.
+              </p>
+            </div>
+          </div>
+          <div class="Slide">
+            <img id="salad" src="../assets/variety.jpg" alt="salad">
+            <div id="variety" class="text">
+              <h4>Something For Everyone </h4>
+              <p>
+                Between our hearty soups, savory salads, and fresh bread, we guarantee that you will find something to suit your taste.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="container">
         <div id="menu">
@@ -40,8 +61,7 @@
           <ul>
             <li>
               <b>Lentil Stew</b>
-              - A classic, hearty mix of lentils and vegetables in a savory
-              broth
+              - A classic, hearty mix of lentils and vegetables in a savory broth
             </li>
             <li>
               <b>Lemon Rosemary White Bean Soup</b>
@@ -53,42 +73,68 @@
               coriander.
             </li>
           </ul>
-          <button type="button">
-            <router-link to="/menu">View Full Menu</router-link>
-          </button>
+          <button type="button"> <router-link to="/menu">View Full Menu</router-link></button>
         </div>
         <div id="hoursH">
           <h2>Operating Hours</h2>
           <ul>
-            <li>Monday</li>
-            <li>8:00 AM - 9:00 PM</li>
+            <li>
+              Monday
+            </li>
+            <li>
+              8:00 AM - 9:00 PM
+            </li>
           </ul>
           <ul>
-            <li>Tuesday</li>
-            <li>8:00 AM - 9:00 PM</li>
+            <li>
+              Tuesday
+            </li>
+            <li>
+              8:00 AM - 9:00 PM
+            </li>
           </ul>
           <ul>
-            <li>Wednesday</li>
-            <li>8:00 AM - 9:00 PM</li>
+            <li>
+              Wednesday
+            </li>
+            <li>
+              8:00 AM - 9:00 PM
+            </li>
           </ul>
           <ul>
-            <li>Thurday</li>
-            <li>8:00 AM - 9:00 PM</li>
+            <li>
+              Thurday
+            </li>
+            <li>
+              8:00 AM - 9:00 PM
+            </li>
           </ul>
           <ul>
-            <li>Friday</li>
-            <li>8:00 AM - 9:00 PM</li>
+            <li>
+              Friday
+            </li>
+            <li>
+              8:00 AM - 9:00 PM
+            </li>
           </ul>
           <ul>
-            <li>Saturday</li>
-            <li>8:00 AM - 10:00 PM</li>
+            <li>
+              Saturday
+            </li>
+            <li>
+              8:00 AM - 10:00 PM
+            </li>
           </ul>
           <ul>
-            <li>Sunday</li>
-            <li>9:30 AM - 8:00 PM</li>
+            <li>
+              Sunday
+            </li>
+            <li>
+              9:30 AM - 8:00 PM
+            </li>
           </ul>
         </div>
-      </div>
+      </div>  
     </body>
   </div>
 </template>
@@ -96,160 +142,191 @@
 <style>
 header {
   background-image: url("../assets/farm.jpeg");
-  background-repeat: repeat-x; /* This repeats the image only horizontally */
-  background-size: cover; /* Ensures the image covers the entire width */
-}
-#about button {
-  justify-self: center;
-  min-width: 100px;
-  height: 50px;
-  font-size: larger;
-  background-color: #a34e14;
-  border: 1px solid #a34e14;
-  border-radius: 10px;
-  padding: 10px;
-  font-family: serif;
-  font-weight: bold;
-  font-style: normal;
-  margin: 10px 0px 20px 0px;
-}
-#about button a {
-  text-decoration: none;
-  color: rgb(231, 191, 142);
-}
-#menu button {
-  min-width: 100px;
-  height: 50px;
-  font-size: larger;
-  background-color: #a34e14;
-  border: 1px solid #a34e14;
-  border-radius: 10px;
-  padding: 10px;
-  font-family: serif;
-  font-weight: bold;
-  font-style: normal;
-  margin: 10px 0px 20px 0px;
-}
-#menu button a {
-  text-decoration: none;
-  color: rgb(231, 191, 142);
-}
-li {
-  list-style-type: none;
-}
-#about {
-  display: grid;
-  grid-template-columns: 50% 50%;
-  max-width: 1000px;
-  margin: 10px auto;
-}
-#about section {
-  padding: 10px;
-  max-width: 500px;
-  margin: 0px auto;
-}
-#about img {
-  margin: auto;
-}
-.text {
-  justify-self: left;
-}
-#about p {
-  text-align: center;
-  font-size: 20px;
-}
-p {
-  margin: 0px 0px 0px 10px;
-  text-align: left;
-}
-#menu ul {
-  text-align: left;
-  margin: 10px 0px 10px 50px;
-}
-#menu ul li {
-  padding: 5px;
-  margin-bottom: 2px;
-}
-#menu h3 {
-  text-align: left;
-  margin-left: 40px;
-}
-#hoursH li {
+    background-repeat: repeat-x; /* This repeats the image only horizontally */
+    background-size: cover; /* Ensures the image covers the entire width */
+  }
+  #about button
+  {
+    justify-self: center;
+    min-width: 100px;
+    height: 50px;
+    font-size: larger;
+    background-color: #A34E14;
+    border: 1px solid #A34E14;
+    border-radius: 10px;
+    padding: 10px;
+    font-family: serif;
+    font-weight: bold;
+    font-style: normal;
+    margin: 10px 0px 20px 0px;
+  }
+  #about button a{
+    text-decoration: none;
+    color: rgb(231, 191,142);
+  }
+  #menu button
+  {
+    min-width: 100px;
+    height: 50px;
+    font-size: larger;
+    background-color: #A34E14;
+    border: 1px solid #A34E14;
+    border-radius: 10px;
+    padding: 10px;
+    font-family: serif;
+    font-weight: bold;
+    font-style: normal;
+    margin: 10px 0px 20px 0px;
+  }
+  #menu button a{
+    text-decoration: none;
+    color: rgb(231, 191,142);
+  }
+  li
+  {
+    list-style-type: none;
+  }
+  #about
+  {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    max-width: 1000px;
+    margin: 10px auto;
+  }
+  #about section
+  {
+    padding: 10px;
+    max-width: 500px;
+    margin: 0px auto;
+  }
+  #about img
+  {
+    margin: auto;
+    width:90%;
+  }
+  .Slide .text
+  {
+    color: white;
+    text-align: left;
+    font-size: 15px;
+    background-color: rgb(63, 73, 54, 0.4);
+    max-width: 500px;
+  }
+  #farm
+  {
+    position: fixed;
+
+  }
+  .text
+  {
+    justify-self: left;
+  }
+  #about #desc
+  {
+    text-align: center;
+    font-size: 20px;
+  }
+  p
+  {
+    margin: 0px 0px 0px 10px;
+    text-align: left;
+  }
+  #menu ul
+  {
+    text-align: left;
+    margin: 10px 0px 10px 50px;
+  }
+  #menu ul li
+  {
+    padding: 5px;
+    margin-bottom: 2px;
+  }
+  #menu h3
+  {
+    text-align: left;
+    margin-left:40px;
+  }
+  #hoursH li {
   list-style-type: none;
   padding: 0;
-  margin: 0.5em;
+  margin: .5em;
 }
 #hoursH ul {
   display: flex;
   justify-content: space-between;
 }
-#hoursH {
-  max-width: 450px;
-}
-h2 {
-  font-family: serif;
-  font-weight: bold;
-  font-style: normal;
-}
-h1 {
-  font-family: serif;
-  font-weight: bold;
-  font-style: normal;
-}
+#hoursH
+  {
+    max-width: 450px;
+  }
+  h2 {
+    font-family: serif;
+    font-weight: bold;
+    font-style: normal;
+  }
+  h1{
+    font-family: serif;
+    font-weight: bold;
+    font-style: normal;
+    font-size: 30px;
+  }
 @media screen and (max-width: 600px) {
-  #about {
+  #about
+  {
     display: block;
   }
-  .container {
+  .container{
     display: block;
   }
-  #hoursH {
-    margin: auto;
+  #hoursH{
+    margin:auto;
   }
 }
-@media screen and (min-width: 600px) and (max-width: 1023px) {
-  .container {
+@media screen and (min-width:600px) and (max-width:1023px) {
+  .container{
     display: grid;
     grid-template-columns: 50% 50%;
   }
-  #hoursH {
+  #hoursH{
     text-align: center;
     justify-content: center;
     max-width: 500px;
     margin: 0px 20px 20px 20px;
-    border: 2px solid rgb(231, 191, 142);
+    border: 2px solid rgb(231, 191,142);
     border-radius: 5px;
   }
-  #menu {
-    max-width: 500px;
+  #menu
+  {
+    max-width:500px;
     justify-content: center;
     margin: 0px 20px 20px 20px;
-    border: 2px solid rgb(231, 191, 142);
+    border: 2px solid rgb(231, 191,142);
     border-radius: 5px;
-    align-content: top;
+    align-content:top;
   }
 }
 @media screen and (min-width: 1023px) {
-  .container {
+  .container{
     display: grid;
     grid-template-columns: 50% 50%;
     justify-content: center;
   }
-  #hoursH {
+  #hoursH{
     text-align: center;
     justify-content: center;
-    align-content: top;
+    align-content:top;
     max-width: 500px;
     margin: 0px 20px 20px 20px;
-    border: 2px solid rgb(231, 191, 142);
+    border: 2px solid rgb(231, 191,142);
     border-radius: 5px;
   }
-  #menu {
-    align-content: top;
-    max-width: 500px;
+  #menu
+  {
+    align-content:top;
+    max-width:500px;
     justify-self: right;
     margin: 0px 20px 20px 20px;
-    border: 2px solid rgb(231, 191, 142);
+    border: 2px solid rgb(231, 191,142);
     border-radius: 5px;
   }
 }
