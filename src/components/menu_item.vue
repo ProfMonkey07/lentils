c33ef222261257a491a302917c8e5f91a780c3c4
ProfMonkey07 <caldermcqueen@gmail.com>
2025-04-02 20:27:34 -0700

    image loading system

--- src/components/menu_item.vue
+++ src/components/menu_item.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { defineProps } from "vue";
-defineProps(["name", "price", "id"]);
+const props = defineProps(["name", "price", "id", "loc"]);
+function getsource() {
+  return require("@/assets/" + props.loc);
+}
 </script>
 
 <template>
   <div class="menuitem">
     <h4>{{ name }} ${{ price }}</h4>
+    <img class="image" :src="getsource()" />
     <button @click="$emit('menuorder', id)">add to cart</button>
   </div>
 </template>
@@ -17,8 +21,19 @@ template {
 .menuitem {
   display: inline-block;
   background-image: url("../assets/paper.png");
-  background-size: cover;
+  background-size: auto;
+  background-repeat: no-repeat;
   border-radius: 10px;
   margin-top: 10px;
+}
+h4 {
+  display: inline-block;
+  vertical-align: top;
+}
+.image {
+  vertical-align: middle;
+  display: inline-block;
+  border-radius: 5px;
+  scale: 20%;
 }
 </style>
