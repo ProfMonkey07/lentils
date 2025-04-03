3ba692627617400298e1ff5b386fa2f5b3578950
Sofia Adair <flowerandbelle@gmail.com>
2025-04-02 21:32:05 -0700

    added vueper to yarn. might work now

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 createApp(App).use(router).mount("#app");
-
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+export default{
+    components: {VueperSlides, VueperSlide}
+}
